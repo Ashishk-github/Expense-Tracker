ed1ab02144fc5400d1a87931574fb4e01093d0ca
Ashish K <ashishskabade@gmail.com>
2022-06-11 13:38:11 +0530

    expense tracker api integrated

--- sample.js
+++ sample.js
@@ -1,50 +1,30 @@
-if(localStorage===null){
-    var z=0;
-}else{
-    var z=0;
-    for(x of Object.keys(localStorage)){
-        if(z<parseInt(x)){
-            z=parseInt(x);
-        }
-    }
-}
-
-function saveToLocalStorage(event) {
+function saveTocrudcrud(event) {
     event.preventDefault();
     const amt = event.target.amt.value;
     const des = event.target.des.value;
     const cat = event.target.cat.value;
-    const obj = {amt,des,cat,z}
-    z++
-    localStorage.setItem(obj.z, JSON.stringify(obj))
-    showExpenseList(obj)
+    const obj = {amt,des,cat}
+    axios.post('https://crudcrud.com/api/4969ccf98cf84542bdb0f8dc292b78ca/Expenses',obj)
+    .then((res)=>showExpenseList(res.data))
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    z++;
-    const localStorageObj = localStorage;
-    const localstoragekeys  = Object.keys(localStorageObj)
-
-    for(var i =0; i< localstoragekeys.length; i++){
-        const key = localstoragekeys[i]
-        const expenseList = localStorageObj[key];
-        const expenseListObj = JSON.parse(expenseList);
-        showExpenseList(expenseListObj)
+    axios.get('https://crudcrud.com/api/4969ccf98cf84542bdb0f8dc292b78ca/Expenses')
+    .then((res)=>{
+    for(var i =0; i< res.data.length; i++){
+        showExpenseList(res.data[i])
     }
-})
+})})
 
 function showExpenseList(expense){
     document.getElementById('des').value = '';
     document.getElementById('amt').value = '';
-    document.getElementById('cat').value ='';
-    if(localStorage.getItem(expense.z) !== null){
-        removeExpense(expense.z)
-    }
+    document.getElementById('cat').value ='Miscellaneous';
 
     const parentNode = document.getElementById('expenseList1');
-    const childHTML = `<li id=${expense.z}> ${expense.amt} - ${expense.des} -${expense.cat}
-                            <button onclick=deleteExpense('${expense.z}')> Delete User </button>
-                            <button onclick=editExpense('${expense.des}','${expense.amt}','${expense.cat}',${expense.z})>Edit User </button>
+    const childHTML = `<li id=${expense._id}> ${expense.amt} - ${expense.des} -${expense.cat}
+                            <button onclick=deleteExp('${expense._id}')><img src="del.png" width=15 depth=15></button>
+                            <button onclick=editExpense('${expense.des}','${expense.amt}','${expense.cat}','${expense._id}')><img src="edit.png" width=15 depth=15></button>
                          </li>`
 
     parentNode.innerHTML = parentNode.innerHTML + childHTML;
@@ -56,15 +36,13 @@ function editExpense(des, amt, cat,z){
     document.getElementById('des').value = des;
     document.getElementById('amt').value = amt;
     document.getElementById('cat').value =cat;
-
-
     deleteExp(z)
  }
 
 
-function deleteExpense(z){
-    console.log(z)
-    localStorage.removeItem(z);
+function deleteExp(z){
+    url='https://crudcrud.com/api/4969ccf98cf84542bdb0f8dc292b78ca/Expenses/'+z
+    axios.delete(url);
     removeExpense(z);
 
 }
